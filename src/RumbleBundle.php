c19a8a900cc21be898b01f524172ec61990eea2c
Yevhen Matasar <yevhen@starofservice.com>
2018-05-31 18:00:47 +0300

    Symfony 4 bundle

---
+++ src/RumbleBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Matasar\Bundle\Rumble;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Yevhen Matasar <matasarei@gmail.com>
+ */
+class RumbleBundle extends Bundle
+{
+    /**
+     * Bundle name.
+     *
+     * @var string
+     */
+    protected $name = 'RumbleBundle';
+}
--- src/Seeder.php
+++ src/Seeder.php
@@ -1,31 +1,35 @@
 <?php
 
-namespace Rumble;
+namespace Matasar\Bundle\Rumble;
 
+/**
+ * @author Lawrence Enehizena <lawstands@gmail.com>
+ */
 abstract class Seeder
 {
     /**
-     *  DynamoDb table
+     * DynamoDb table
      */
     private $table;
 
     /**
-     *   Aws dynamoDBClient
+     * Aws dynamoDBClient
      */
     private $dynamoDBClient;
 
     /**
-     *  Aws Marshaler(makes formating data easy)
+     * Aws Marshaler(makes formating data easy)
      */
     private $marshaler;
 
     /**
-     *  dynamoDB table items
+     * dynamoDB table items
      */
     private $items = [];
 
     /**
      * Seeder constructor.
+     *
      * @param $dynamoDBClient
      * @param $marshaler
      */
@@ -37,16 +41,19 @@ abstract class Seeder
 
     /**
      * @param $name
+     *
      * @return $this
      */
     protected function table($name)
     {
         $this->table = $name;
+
         return $this;
     }
 
     /**
      * @param $data
+     *
      * @return $this
      */
     protected function addItem($data)
@@ -58,31 +65,38 @@ abstract class Seeder
            $item[$attribute] = $value;
         }
         $this->items[] = $item;
+
         return $this;
     }
 
     /**
      * Verify that the TableName param is set.
      * This is a mandatory param, just like the hash key.
+     *
      * @throws \Exception
      */
     private function isTableNameSet()
     {
-        if (!$this->table)
+        if (!$this->table) {
             throw new \Exception('Error: DynamoDB requires table name to be specified.');
+        }
     }
 
     /**
-     *   Verify that at least one item is added.
+     * Verify that at least one item is added.
+     *
+     * @throws \Exception
      */
     private function atLeastOneItemExist()
     {
-        if (count($this->items) == 0)
+        if (count($this->items) == 0) {
             throw new \Exception('Error: No data to be seeded.');
+        }
     }
 
     /**
      * @param $table
+     *
      * @throws \Exception
      */
     private function tableExist($table)
@@ -94,7 +108,6 @@ abstract class Seeder
         }
     }
 
-
     /**
      * Check if there are multiple items.
      */
@@ -105,28 +118,36 @@ abstract class Seeder
 
     /**
      * Check to make sure batch items is not over limit (100).
+     *
+     * @throws \Exception
      */
     private function validateBatchItemsLimit()
     {
-        if(count($this->items) > 100)
+        if (count($this->items) > 100) {
             throw new \Exception('Maximum items that can be bacthed add is 100, limit exceeded.');
+        }
     }
 
     /**
-     *  Show completion message, when migration is successful.
+     * Show completion message, when migration is successful.
      */
     private function displayCompletionMessage()
     {
         $className = get_class($this);
         echo "{$className} seeded successfully". PHP_EOL;
+
         return true;
     }
 
     /**
-     *  Add a new item(s) to the specified dynamodb table.
-     *  If a single item is added, a simple putItem is used.
-     *  If multiple items are added, then the batchWriteItem is used.
-     *  NB: batchWriteItem can take maximum of 100 items per call.
+     * Add a new item(s) to the specified dynamodb table.
+     * If a single item is added, a simple putItem is used.
+     * If multiple items are added, then the batchWriteItem is used.
+     * NB: batchWriteItem can take maximum of 100 items per call.
+     *
+     * @return bool
+     *
+     * @throws \Exception
      */
     protected function save()
     {
@@ -159,11 +180,12 @@ abstract class Seeder
                 ]
             );
         }
+
         return $this->displayCompletionMessage();
     }
 
     /**
-     *  Force seed files to define the seed method.
+     * Force seed files to define the seed method.
      */
     protected abstract function seed();
 }
